79d72524a01aadb1703698ed8ab42a00afb8d848
Denis <besicdenis@gmail.com>
2016-09-23 06:29:18 +0200

    Adding first test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "title is required"  do
+  	category = Category.new
+  	assert_not category.save, "Save the category without a title"
+  end
 end
